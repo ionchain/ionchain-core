3ed729dfa013200460471fbca3e02faa81395e68
gcc2ge <1414786333@qq.com>
2019-02-27 17:45:36 +0800

    container

--- cmd/ionc/config.go
+++ cmd/ionc/config.go
@@ -9,7 +9,7 @@
 // go-ionchain is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
+// GNU General Pubtlic License for more details.
 //
 // You should have received a copy of the GNU General Public License
 // along with go-ionchain. If not, see <http://www.gnu.org/licenses/>.
